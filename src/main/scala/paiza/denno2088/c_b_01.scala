015993f448bbe34833f6233409a2bc3558f1fe8d
smeghead <smeghead7@gmail.com>
2025-01-26 23:25:05 +0900

    use Point

--- src/main/scala/paiza/denno2088/c_b_01.scala
+++ src/main/scala/paiza/denno2088/c_b_01.scala
@@ -10,13 +10,13 @@ object c_b_01 {
         Board(lines.length, matrix)
     }
 
-    def exist(b: Board, x: Int, y: Int): Boolean = b.matrix(x)(y)
+    def exist(b: Board, p: Point): Boolean = b.matrix(p.x)(p.y)
 
     def startPoints(b: Board): IndexedSeq[Point] = {
         val points = for (
             x <- 0 until b.n - 1;
             y <- 0 until b.n - 1
         ) yield Point(x, y)
-        points.filter(p => exist(b, p.x, p.y))
+        points.filter(p => exist(b, p))
     }
 }
--- src/test/scala/paiza/denno2088/c_b_01Test.scala
+++ src/test/scala/paiza/denno2088/c_b_01Test.scala
@@ -6,15 +6,15 @@ class c_b_01Test extends munit.FunSuite {
   test("面の初期化") {
     val board = c_b_01.createBoard(Array(".#.", "###", ".#."))
 
-    assertEquals(c_b_01.exist(board, 0, 0), true)
-    assertEquals(c_b_01.exist(board, 0, 1), false)
-    assertEquals(c_b_01.exist(board, 0, 2), true)
-    assertEquals(c_b_01.exist(board, 1, 0), false)
-    assertEquals(c_b_01.exist(board, 1, 1), false)
-    assertEquals(c_b_01.exist(board, 1, 2), false)
-    assertEquals(c_b_01.exist(board, 2, 0), true)
-    assertEquals(c_b_01.exist(board, 2, 1), false)
-    assertEquals(c_b_01.exist(board, 2, 2), true)
+    assertEquals(c_b_01.exist(board, c_b_01.Point(0, 0)), true)
+    assertEquals(c_b_01.exist(board, c_b_01.Point(0, 1)), false)
+    assertEquals(c_b_01.exist(board, c_b_01.Point(0, 2)), true)
+    assertEquals(c_b_01.exist(board, c_b_01.Point(1, 0)), false)
+    assertEquals(c_b_01.exist(board, c_b_01.Point(1, 1)), false)
+    assertEquals(c_b_01.exist(board, c_b_01.Point(1, 2)), false)
+    assertEquals(c_b_01.exist(board, c_b_01.Point(2, 0)), true)
+    assertEquals(c_b_01.exist(board, c_b_01.Point(2, 1)), false)
+    assertEquals(c_b_01.exist(board, c_b_01.Point(2, 2)), true)
   }
 
   test("探索開始地点のリストアップ") {
