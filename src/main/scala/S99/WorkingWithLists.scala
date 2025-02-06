d321c6b46ae74152a13414b7ddef9e1c355085b2
smeghead <smeghead7@gmail.com>
2025-02-06 22:25:55 +0900

    答を確認して改善しました。

--- src/main/scala/S99/WorkingWithLists.scala
+++ src/main/scala/S99/WorkingWithLists.scala
@@ -1,7 +1,19 @@
 package S99
 
 object WorkingWithLists {
-  def last(xs: List[Int]): Int = {
-    xs.reverse.head
+  // P01  (*) Find the last element of a list.
+  def last[A](xs: List[A]): A = xs match {
+    case h :: Nil => h
+    case _ :: tail => last(tail)
+    case _ => throw new NoSuchElementException
   }
+
+  // def last[A](xs: List[A]): A = xs.last
+
+  // def last(xs: List[Int]): Int = {
+  //   xs.reverse.head
+  // }
+
+  // P02 (*) Find the last but one element of a list.
+  def penultimate(xs: List[Int]): Int = ???
 }
--- src/test/scala/S99/WorkingWithListsTest.scala
+++ src/test/scala/S99/WorkingWithListsTest.scala
@@ -6,4 +6,8 @@ class WorkingWithListsTest extends munit.FunSuite {
   test("P01 (*) Find the last element of a list.") {
     assertEquals(8, last(List(1, 1, 2, 3, 5, 8)))
   }
+
+  // test("P02 (*) Find the last but one element of a list.") {
+  //   assertEquals(5, penultimate(List(1, 1, 2, 3, 5, 8)))
+  // }
 }
